587887b9d1822111a64400a73f600e0b034f17d1
luann <luanna.v.quinalha@gmail.com>
2023-03-15 14:57:26 -0300

    added option to search by different columns

--- camara/script.js
+++ camara/script.js
@@ -29,10 +29,37 @@ function myFunction() {
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
+
+    var j;
+    j=0;
+    if (busca=='temas'){
+      j=0;
+    }
+    else if (busca=='ementa'){
+      j=1;
+    }
+    else if (busca=='tipo'){
+      j=2;
+    }
+    else if (busca=='ano'){
+      j=3;
+    }
+    else if (busca=='autor'){
+      j=4;
+    }
+    else if (busca=='cargo'){
+      j=5;
+    }
+    else if (busca=='partido'){
+      j=6;
+    }
+    else if (busca=='acompanhe'){
+      j=7;
+    }
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
+      td = tr[i].getElementsByTagName("td")[j];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -43,3 +70,46 @@ function myFunction() {
       }
     }
   }
+
+
+
+
+  /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+var busca;
+
+function temas(){
+  busca='temas';
+}
+
+function ementa(){
+  busca='ementa';
+}
+
+function tipo(){
+  busca='tipo';
+}
+
+function ano(){
+  busca='ano';
+}
+
+function autor(){
+  busca='autor';
+}
+
+function cargo(){
+  busca='cargo';
+}
+
+function partido(){
+  busca='partido';
+}
+
+function acompanhe(){
+  busca='acompanhe';
+}
--- senado/script.js
+++ senado/script.js
@@ -30,9 +30,36 @@ function myFunction() {
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
   
+    var j;
+    j=0;
+    if (busca=='proposta'){
+      j=0;
+    }
+    else if (busca=='temas'){
+      j=1;
+    }
+    else if (busca=='ementa'){
+      j=2;
+    }
+    else if (busca=='ano'){
+      j=3;
+    }
+    else if (busca=='autor'){
+      j=4;
+    }
+    else if (busca=='cargo'){
+      j=5;
+    }
+    else if (busca=='partido'){
+      j=6;
+    }
+    else if (busca=='acompanhe'){
+      j=7;
+    }
+
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
+      td = tr[i].getElementsByTagName("td")[j];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -42,4 +69,42 @@ function myFunction() {
         }
       }
     }
+  }
+
+  function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  var busca;
+  
+  function proposta(){
+    busca='proposta';
+  }
+  
+  function temas(){
+    busca='temas';
+  }
+  
+  function ementa(){
+    busca='ementa';
+  }
+  
+  function ano(){
+    busca='ano';
+  }
+  
+  function autor(){
+    busca='autor';
+  }
+  
+  function cargo(){
+    busca='cargo';
+  }
+  
+  function partido(){
+    busca='partido';
+  }
+  
+  function acompanhe(){
+    busca='acompanhe';
   }
